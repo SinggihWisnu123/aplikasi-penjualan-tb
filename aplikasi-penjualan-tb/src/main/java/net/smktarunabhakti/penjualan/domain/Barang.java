5c048b36172d28eaa4799f6106a95ae841b6181d
SinggihWisnu123 <singgihwisnupranata@gmail.com>
2014-05-16 17:29:38 +0700

    menambah file lengkap edisi baru

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/Barang.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/Barang.java
@@ -11,45 +11,47 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
 
-
 @Entity
-@Table(name="m_barang")
-public class Barang implements Serializable{
-	
-	@Id
-	@GeneratedValue(generator = "system-uuid")
-	@GenericGenerator(name = "system-uuid",strategy = "uuid2")
-	private String id;
-	
-	@NotNull
-	@Column(name="kode_barang",nullable=false,length= 5)
-	// varchar(5),default dari string adalah varchar(255)
-	private String kodeBarang;
-	
-	@NotNull
-	@Column(name="nama_barang",nullable=false)
-	// varchar(255),
-	private String namaBarang;
-	
+@Table(name = "m_barang")
+public class Barang implements Serializable {
 
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getKodebarang() {
-		return kodeBarang;
-	}
-	public void setKodebarang(String kodebarang) {
-		this.kodeBarang = kodebarang;
-	}
-	public String getNamabarang() {
-		return namaBarang;
-	}
-	public void setNamabarang(String namabarang) {
-		this.namaBarang = namabarang;
-	}
-	
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    @Column(name = "kode_barang", nullable = false, length = 5)
+    // varchar(5),default dari string adalah varchar(255)
+    private String kodeBarang;
+
+    @NotNull
+    @Column(name = "nama_barang", nullable = false)
+    // varchar(255),
+    private String namaBarang;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
 }
