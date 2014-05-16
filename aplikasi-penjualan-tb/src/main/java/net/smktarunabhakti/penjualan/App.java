6403088a9b1e457cc645a41e1034538ea28e52eb
SinggihWisnu123 <singgihwisnupranata@gmail.com>
2014-05-16 15:58:48 +0700

    file applicationContext

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -14,6 +14,6 @@ public class App {
     
     public static void main( String[] args ){
         appCtx = new ClassPathXmlApplicationContext("classpath:net/smktarunabhakti/penjualan/"
-                 + "spllicationContext/xml");
+                 + "applicationContext.xml");
     }
 }
