29c0d9f3e8629ff9977cf156dde0926fdd5132b3
SinggihWisnu123 <singgihwisnupranata@gmail.com>
2014-05-16 11:04:37 +0700

    menambahkan file pom.xml dan app.java

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,14 +1,19 @@
 package net.smktarunabhakti.penjualan;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    
+    private static ApplicationContext appCtx;
+    
+    public static void main( String[] args ){
+        appCtx = new ClassPathXmlApplicationContext("classpath:net/smktarunabhakti/penjualan/"
+                 + "spllicationContext/xml");
     }
 }
